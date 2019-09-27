be23a6c90b7c3bc22cc17d9ac6a0b0f50ce0e3f1
Arseniy Kononov <kononovarseniy@gmail.com>
2019-09-27 17:49:52 +0700

    Task #2. Options

--- 2. WOL/main.c
+++ 2. WOL/main.c
@@ -3,7 +3,7 @@
 #include <sys/socket.h>
 #include <sys/ioctl.h>
 #include <netpacket/packet.h> //sockaddr_ll
-//#include <netinet/ether.h>
+#include <netinet/ether.h> // ether_aton, ether_addr
 #include <netinet/in.h> // htons
 #include <net/ethernet.h> // ether_header
 #include <net/if.h> // if_nametoindex
@@ -19,13 +19,51 @@
 #define PACKET_SIZE (ETH_HLEN + WOL_PACKET_SIZE)
 #define ETH_P_WOL 0x0842
 
-int main(int argc, const char *argv[]) {
-    uint8_t src[ETH_ALEN] = { 0x40, 0x16, 0x7e, 0x84, 0x41, 0x59 },
-            dst[ETH_ALEN] = { 0x40, 0x16, 0x7e, 0x84, 0x41, 0x59 };
-    //memset(src, 0, ETH_ALEN);
-    //memset(dst, 0, ETH_ALEN);
+int main(int argc, char *argv[]) {
+    char *ifname = NULL;
+    struct ether_addr src, dst;
+    int src_set = 0, dst_set = 0;
 
-    const char *ifname = "enp5s0";
+    int opt;
+    while ((opt = getopt(argc, argv, ":i:s:d:")) != -1) {
+        struct ether_addr *addr;
+        switch (opt) {
+            case 'i':
+                ifname = strdup(optarg);
+                break;
+            case 's':
+                if ((addr = ether_aton(optarg)) == NULL) {
+                    fprintf(stderr, "Source address is invalid\n");
+                    exit(1);
+                }
+                src = *addr;
+                src_set = 1;
+                break;
+            case 'd':
+                if ((addr = ether_aton(optarg)) == NULL) {
+                    fprintf(stderr, "Destination address is invalid\n");
+                    exit(1);
+                }
+                dst = *addr;
+                dst_set = 1;
+                break;
+            case ':': // Missing arg
+                fprintf(stderr, "Missing argument for option -%c\n", (char)optopt);
+                exit(1);
+            case '?': // Unknown option
+                fprintf(stderr, "Unknown option -%c\n", (char)optopt);
+                exit(1);
+        }
+    }
+    if (ifname == NULL) {
+        fprintf(stderr, "Interface is not set\n");
+        exit(1);
+    }
+    if (!src_set || ! dst_set) {
+        fprintf(stderr, "Source or destination is not set\n");
+        exit(1);
+    }
+
     unsigned int if_index = if_nametoindex(ifname);
     if (if_index == 0) {
         perror("if_nametosocket failed");
@@ -38,23 +76,25 @@ int main(int argc, const char *argv[]) {
     addr.sll_family = AF_PACKET;
     addr.sll_halen = ETH_ALEN;
     addr.sll_ifindex = if_index; 
-    memcpy(&addr.sll_addr, dst, ETH_ALEN);
+    memcpy(&addr.sll_addr, &dst, ETH_ALEN);
     printf("Socket address structure initialized\n");
 
     uint8_t buf[PACKET_SIZE], *ptr = buf;
     struct ether_header *hdr = (struct ether_header *)ptr;
 
-    memcpy(hdr->ether_dhost, dst, ETH_ALEN);
-    memcpy(hdr->ether_shost, src, ETH_ALEN);
+    memcpy(hdr->ether_dhost, &dst, ETH_ALEN);
+    memcpy(hdr->ether_shost, &src, ETH_ALEN);
     hdr->ether_type = htons(ETH_P_WOL);
     ptr += ETH_HLEN;
     printf("Ethernet header initialized\n");
 
-    for (int i = 0; i < WOL_PREAMBLE_LEN; i++)
-        *ptr++ = 0xff;
+    memset(ptr, 0xff, WOL_PREAMBLE_LEN);
+    ptr += WOL_PREAMBLE_LEN;
 
-    for (int i = 0; i < WOL_ADDR_REP * ETH_ALEN; i++)
-        *ptr++ = dst[i % ETH_ALEN];
+    for (int i = 0; i < WOL_ADDR_REP; i++) {
+        memcpy(ptr, &dst, ETH_ALEN);
+        ptr += ETH_ALEN;
+    }
     printf("WOL payload written\n");
 
     // Open socket, send a packet and close the socket.
